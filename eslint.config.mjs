dcd3fe07d8eb66314bfcb38272a20210088d5aaf
Alexis Coelho <alexis.coelho@netcentric.biz>
2024-10-18 16:45:12 +0200

    Small improvements #68

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,38 +7,34 @@ export default [
     files: ['**/*.js', '**/*.jsx', '**/*.mjs'],
   },
   {
-    ignores: [
-      '**/*.min.js',
-      'tools/importer/',
-    ]
+    ignores: ['**/*.min.js', 'tools/importer/'],
   },
   {
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
   prettierRecommended,
   {
     rules: {
-      indent: ["error", 2],
-      "comma-dangle": ["error", "always-multiline"],
-      "comma-spacing": ["error", { "before": false, "after": true }],
-      "comma-style": ["error", "last"],
-      "no-extra-semi": "error",
-      "no-tabs": "error",
-      "func-names": ["error", "as-needed"],
-      semi: ["error", "always"],
-      "no-nested-ternary": "error",
-      "no-new": "error",
-      "no-unused-vars": "warn",
-      "no-duplicate-imports": ["error", { "includeExports": true }],
-      "one-var": ["error", "never"],
-      "no-var": "error",
-      "prefer-const": "error",
-      "prefer-template": "error",
-      "no-multiple-empty-lines": "error",
-      quotes: ["error", "single"],
-      "no-constant-binary-expression": "error"
-    }
-  }
-  
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'comma-spacing': ['error', { before: false, after: true }],
+      'comma-style': ['error', 'last'],
+      'no-extra-semi': 'error',
+      'no-tabs': 'error',
+      'func-names': ['error', 'as-needed'],
+      semi: ['error', 'always'],
+      'no-nested-ternary': 'error',
+      'no-new': 'error',
+      'no-unused-vars': 'warn',
+      'no-duplicate-imports': ['error', { includeExports: true }],
+      'one-var': ['error', 'never'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
+      'no-multiple-empty-lines': 'error',
+      quotes: ['error', 'single'],
+      'no-constant-binary-expression': 'error',
+    },
+  },
 ];
