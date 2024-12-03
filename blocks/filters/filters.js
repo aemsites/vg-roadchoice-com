24b021d40a7b80eb219df28c57fc4a6523482a39
Luka Stanisavljević <luka@cincplug.com>
2024-12-03 11:51:07 +0100

    FR: opensearch #69 (#100)
    * Load graphql results when cat is not in URL query
    * Adjust pagination logic
    * Add filters
    * Load old category filters
    * Prevent title rewrite with no-results title
    * Prevent old render when no category
    * Remove scroll
    * Remove show more when graphql sends less than limit
    * Extend previous fix to zero offset case
    * Use graphql for category search too
    * Remove obsolete parts of old search
    * Get part search options from GraphQL
    * Remove code not used anymore
    * Reorganize code
    * Use clearer file name
    * Prevent undefined in title
    * Consolidate confiG
    * Abstract the try-catch logic
    * Move config to constants file
    * Validate DOM selectors and refactor big function
    * Distinguish page and offset
    * Fix offset undefined
    * Improve null condition
    
    ---------
    
    Co-authored-by: Luka Stanisavljevic <luka.stanisavljevic@cognizant.com>

--- blocks/filters/filters.js
+++ blocks/filters/filters.js
@@ -1,36 +1,9 @@
 import { getTextLabel, createElement } from '../../scripts/common.js';
 
 const blockName = 'filters';
-let products;
-const isSearchResult = document.querySelector('.search-results') !== null;
-const urlCategory = new URLSearchParams(window.location.search).get('cat');
-const total = +sessionStorage.getItem('total-results-amount') || null;
 const categoriesText = getTextLabel('categories_label');
 
-if (isSearchResult) products = JSON.parse(sessionStorage.getItem('results')) || [];
-
-const reduceArrays = (array) => {
-  const initialValue = {};
-  const reduced = array.reduce((acc, value) => ({ ...acc, [value]: (acc[value] || 0) + 1 }), initialValue);
-  return reduced;
-};
-
-const reduceCategories = (cats) => {
-  const categoryList = cats.map((x) => x['Part Category'].toLowerCase());
-  const catToReduce = urlCategory ? categoryList.filter((item) => item.toLowerCase() === urlCategory) : categoryList;
-  const reducedCategories = reduceArrays(catToReduce);
-  const orderedCategories = Object.keys(reducedCategories)
-    .sort()
-    .reduce((obj, key) => {
-      obj[key] = reducedCategories[key];
-      return obj;
-    }, {});
-  const reducedArray = Object.entries(orderedCategories);
-
-  return reducedArray;
-};
-
-const buildFilter = (cats) => {
+export const buildFilter = (cats) => {
   const section = createElement('div', { classes: `${blockName}-section` });
   const title = createElement('h3', { classes: 'title' });
   title.textContent = categoriesText;
@@ -40,7 +13,7 @@ const buildFilter = (cats) => {
 
   cats.forEach((cat) => {
     const [category, amount] = cat;
-    urlParams.set('cat', category.toLowerCase());
+    urlParams.set('category', category.toLowerCase());
     const filterUrl = `${currentUrl.pathname}?${urlParams.toString()}`;
     const item = createElement('li', { classes: 'item' });
     const link = createElement('a', {
@@ -56,24 +29,8 @@ const buildFilter = (cats) => {
   return section;
 };
 
-const decorateFilter = (block) => {
-  const filtersSection = createElement('div', { classes: `${blockName}-wrapper` });
-
-  const categories = reduceCategories(products);
-
-  const categoryFilterSection = buildFilter(categories);
-
-  filtersSection.append(categoryFilterSection);
-
-  block.textContent = '';
-  block.append(filtersSection);
-};
-
 export default async function decorate(block) {
-  document.addEventListener('DataLoaded', ({ detail }) => {
-    products = detail.results;
-    if (products.length > 0) decorateFilter(block);
-  });
-
-  if (products.length > 0 && !total) decorateFilter(block);
+  const filtersWrapper = createElement('div', { classes: `${blockName}-wrapper` });
+  block.textContent = '';
+  block.append(filtersWrapper);
 }
--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -1,102 +1,26 @@
 import { getTextLabel, createElement } from '../../scripts/common.js';
-import { amountOfProducts } from '../search/search.js';
+import { getAndApplySearchResults } from '../search/search.js';
 
 const blockName = 'pagination';
-const amount = JSON.parse(sessionStorage.getItem('amount')) || amountOfProducts;
-let products = JSON.parse(sessionStorage.getItem('results')) || [];
-let moreBtns = [];
-let currentAmount = 0;
-let hasMoreItems = false;
-let newText = '';
-let isDecorated = false;
-let imageData = [];
 const partNumberText = getTextLabel('part_number');
-const displayedTextContent = getTextLabel('pagination_text');
 const buttonTextContent = getTextLabel('pagination_button');
 const firstWord = partNumberText.split(' ')[0];
-const category = new URLSearchParams(window.location.search).get('cat');
 
-const loadMoreProducts = (props) => {
-  const { hidden, amountText } = props;
-  const { length } = hidden;
-  const isLessThanAmount = length <= amount;
-  const nextAmount = isLessThanAmount ? length : amount;
-  currentAmount += nextAmount;
-
-  for (let i = 0; i < nextAmount; i += 1) {
-    hidden[i].classList.remove('hidden');
-  }
-
-  amountText.textContent = displayedTextContent.replace('[$]', currentAmount);
-
-  if (isLessThanAmount) moreBtns.forEach((btn) => btn.classList.add('hidden'));
-};
-
-const addShowMoreHandler = (btn, resultsListBlock, amountText) => {
-  btn.onclick = () =>
-    loadMoreProducts({
-      hidden: resultsListBlock.querySelectorAll('.product-card.hidden'),
-      amountText,
-    });
-};
-
-const addButtons = ({ resultsListBlock, moreBtn, bottomMoreBtn }) => {
-  resultsListBlock.querySelector('.results-list__section').appendChild(bottomMoreBtn);
-  moreBtn.classList.remove('hidden');
-  moreBtns = [moreBtn, bottomMoreBtn];
-};
-
-const decoratePagination = (block) => {
+export default async function decorate(block) {
   const paginationSection = createElement('div', { classes: `${blockName}-section` });
   const paginationTitle = createElement('h2', { classes: 'title' });
   paginationTitle.textContent = `${firstWord}s`;
   const showingSection = createElement('div', { classes: 'showing-section' });
-  const displayedText = createElement('p', { classes: 'displayed-text' });
-  if (category) {
-    products = products.filter((item) => item['Part Category'].toLowerCase() === category);
-  }
-  hasMoreItems = products && products.length > amount;
-  currentAmount = hasMoreItems ? amount : [...products].length;
-  newText = displayedTextContent.replace('[$]', currentAmount);
-  displayedText.textContent = newText;
-  showingSection.append(displayedText);
+  const topResultsElement = createElement('p', { classes: 'top-results-text' });
+  showingSection.append(topResultsElement);
 
-  if (hasMoreItems) {
-    const moreBtn = createElement('button', { classes: ['more-button', 'hidden'] });
-    moreBtn.textContent = buttonTextContent;
-    const bottomMoreBtn = createElement('button', { classes: ['more-button', 'bottom-more-button'] });
-    bottomMoreBtn.textContent = buttonTextContent;
-    const resultsListBlock = document.querySelector('.results-list.block');
-    addShowMoreHandler(moreBtn, resultsListBlock, displayedText);
-    addShowMoreHandler(bottomMoreBtn, resultsListBlock, displayedText);
-    showingSection.append(moreBtn);
-
-    if (imageData.length > 0) {
-      addButtons({ resultsListBlock, moreBtn, bottomMoreBtn });
-    } else {
-      document.addEventListener('DataLoaded', () => {
-        addButtons({ resultsListBlock, moreBtn, bottomMoreBtn });
-      });
-    }
-  }
+  const moreBtnClasses = ['more-button'];
+  const moreBtn = createElement('button', { classes: moreBtnClasses });
+  moreBtn.textContent = buttonTextContent;
+  showingSection.append(moreBtn);
+  moreBtn.onclick = () => getAndApplySearchResults({ isFirstSet: false });
 
   paginationSection.append(paginationTitle, showingSection);
 
   block.append(paginationSection);
-};
-
-export default async function decorate(block) {
-  document.addEventListener('DataLoaded', ({ detail }) => {
-    products = detail.results;
-    imageData = detail.data.imgData;
-    if (!isDecorated) {
-      isDecorated = true;
-      if (products.length > 0) decoratePagination(block);
-    }
-  });
-
-  if (sessionStorage.getItem('results') && !isDecorated) {
-    isDecorated = true;
-    if (products.length > 0) decoratePagination(block);
-  }
 }
