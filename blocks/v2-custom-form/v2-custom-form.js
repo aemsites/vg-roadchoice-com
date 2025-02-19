ab21d4935489b2b0536d45b4947bd9656f48dbad
amglebihan <166131459+amglebihan@users.noreply.github.com>
2025-02-19 09:31:25 +0100

    121 - Add cleanErrorMessages function

--- blocks/v2-custom-form/v2-custom-form.js
+++ blocks/v2-custom-form/v2-custom-form.js
@@ -459,6 +459,16 @@ function decorateValidation(form) {
   });
 }
 
+function cleanErrorMessages(form) {
+  const spanErrors = form.querySelectorAll('span.error');
+  spanErrors.forEach((span) => {
+    const parentEl = span.closest('.field-wrapper.invalid');
+    if (!parentEl) {
+      span.remove();
+    }
+  });
+}
+
 async function createForm(formURL) {
   const { pathname } = new URL(formURL);
   const data = await fetchForm(pathname);
@@ -507,6 +517,8 @@ async function createForm(formURL) {
     if (form.hasAttribute('novalidate')) {
       isValid = form.checkValidity();
     }
+    // After submission, the form should clear the error messages if the fields are valid.
+    cleanErrorMessages(form);
     e.preventDefault();
     if (isValid) {
       e.submitter.setAttribute('disabled', '');
