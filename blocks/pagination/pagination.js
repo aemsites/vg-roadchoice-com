96510de0baeace231ecf76171dbf29bc1eed70f9
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2025-02-26 08:56:29 +0100

    Part word comes from placeholder.json && search url fix #101

--- blocks/pagination/pagination.js
+++ blocks/pagination/pagination.js
@@ -2,14 +2,13 @@ import { getTextLabel, createElement } from '../../scripts/common.js';
 import { getAndApplySearchResults } from '../search/search.js';
 
 const blockName = 'pagination';
-const partNumberText = getTextLabel('part_number');
 const buttonTextContent = getTextLabel('pagination_button');
-const firstWord = partNumberText.split(' ')[0];
+const partWord = getTextLabel('part');
 
 export default async function decorate(block) {
   const paginationSection = createElement('div', { classes: `${blockName}-section` });
   const paginationTitle = createElement('h2', { classes: 'title' });
-  paginationTitle.textContent = `${firstWord}s`;
+  paginationTitle.textContent = `${partWord}s`;
   const showingSection = createElement('div', { classes: 'showing-section' });
   const topResultsElement = createElement('p', { classes: 'top-results-text' });
   showingSection.append(topResultsElement);
