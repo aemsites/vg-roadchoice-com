9b7679513b62f07e0272cf6b009635047cf08f8f
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2025-04-14 14:53:29 +0200

    just add quotes in the no results query term

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -219,7 +219,7 @@ const updatePagination = (resultsSection, targetOffset, resultsLength) => {
 
 const showNoResultsMessage = (query, searchResultsSection) => {
   const titleElement = searchResultsSection?.querySelector('.title');
-  const titleText = getTextLabel('no_results_title').replace('[$]', query ? `${query}` : '');
+  const titleText = getTextLabel('no_results_title').replace('[$]', query ? `"${query}"` : '');
   if (titleElement) titleElement.innerText = titleText;
   const fragment = document.createRange().createContextualFragment(noResultsTemplate);
   searchResultsSection?.classList.add('no-results');
