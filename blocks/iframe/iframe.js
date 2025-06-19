007ba3d289d90fdf8099bd082f852ebcf4192b0d
amglebihan <166131459+amglebihan@users.noreply.github.com>
2025-06-19 17:47:56 +0200

    172 - Add iframe fullscreen variant

--- blocks/iframe/iframe.js
+++ blocks/iframe/iframe.js
@@ -1,11 +1,41 @@
-import { createElement } from '../../scripts/common.js';
+import { createElement, variantsClassesToBEM } from '../../scripts/common.js';
+
+const blockName = 'iframe';
+const variantClasses = ['fullscreen'];
+
+const getIframeSrc = (block) => block.querySelector('a')?.getAttribute('href') || '';
+
+const getFixedHeight = (block) => [...block.classList].find((cls) => /^[0-9]+px$/.test(cls));
+
+const enableFullscreenMode = (block) => {
+  const container = block.closest(`.${blockName}-container`);
+  if (container) {
+    container.classList.add(`${blockName}-container--fullscreen`);
+  }
+  document.body.classList.add(`${blockName}-fullscreen`);
+};
 
 export default async function decorate(block) {
-  const link = block.querySelector('a')?.getAttribute('href');
-  const iframe = createElement('iframe', { props: { src: link, loading: 'lazy', frameborder: 0 } });
-  const fixedHeightClass = [...block.classList].find((el) => /[0-9]+px/.test(el));
-  if (fixedHeightClass) {
-    iframe.height = fixedHeightClass;
+  variantsClassesToBEM(block.classList, variantClasses, blockName);
+
+  const isFullscreen = block.classList.contains(`${blockName}--fullscreen`);
+  const src = getIframeSrc(block);
+
+  const iframe = createElement('iframe', {
+    props: {
+      src,
+      loading: 'lazy',
+      frameborder: 0,
+    },
+  });
+
+  const fixedHeight = getFixedHeight(block);
+  if (fixedHeight && !isFullscreen) {
+    iframe.height = fixedHeight;
+  }
+
+  if (isFullscreen) {
+    enableFullscreenMode(block);
   }
 
   block.replaceChildren(iframe);
