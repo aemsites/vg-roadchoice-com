129ede72a2e614f7673ee96e7df253a325d6a97a
amglebihan <166131459+amglebihan@users.noreply.github.com>
2025-10-01 16:10:26 +0200

    205 - Rename variable

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -109,7 +109,7 @@ function addSearchByListeners(wrapper, form) {
 
     if (e.target.classList.contains(`${blockName}__cross-reference__btn`)) {
       document.querySelector(`.${blockName}__fuzzysearch-results-wrapper`)?.remove();
-      document.querySelector(`.${blockName}__fuzzysearch-title`)?.remove();
+      document.querySelector(`.${blockName}__no-results-title`)?.remove();
     }
   };
 }
