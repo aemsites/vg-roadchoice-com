b75de393216ca4d64ccbe5df761bfaf968b2d843
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2025-10-10 17:26:48 +0200

    current graphQL queries updated to use tenant and language #273

--- blocks/search/graphql-api.js
+++ blocks/search/graphql-api.js
@@ -1,4 +1,4 @@
-import { SEARCH_CONFIG } from '../../scripts/common.js';
+import { SEARCH_CONFIG, getPageLanguage } from '../../scripts/common.js';
 
 async function fetchGraphQLData(graphqlQuery, endpoint) {
   try {
@@ -28,22 +28,51 @@ async function fetchGraphQLData(graphqlQuery, endpoint) {
   }
 }
 
-export async function fetchSearchResults({ query, offset, make, model, searchType, category, applyFuzziness }) {
-  const { SEARCH_URL_DEV, CROSS_REFERENCE_QUERY_NAME, PART_NUMBER_QUERY_NAME, MAX_PRODUCTS_PER_QUERY } = SEARCH_CONFIG;
+/**
+ * Fetch search results from the GraphQL API based on the provided parameters.
+ * @param {Object} params - The search parameters.
+ * @param {string} params.query - The search query string.
+ * @param {number} params.offset - The offset for pagination.
+ * @param {string} [params.make] - The make filter (optional).
+ * @param {string} [params.model] - The model filter (optional).
+ * @param {string} params.searchType - The type of search ('cross' or 'parts').
+ * @param {string} [params.category] - The category filter (optional).
+ * @param {boolean} [params.applyFuzziness] - Whether to apply fuzziness (optional).
+ * @param {string} params.language - The language code for localization.
+ * @returns {Object} An object containing the search results and categories.
+ */
+export async function fetchSearchResults({ query, offset, make, model, searchType, category, applyFuzziness, language = getPageLanguage() }) {
+  const { SEARCH_URL_DEV, CROSS_REFERENCE_QUERY_NAME, PART_NUMBER_QUERY_NAME, MAX_PRODUCTS_PER_QUERY, TENANT } = SEARCH_CONFIG;
 
   const queryName = searchType === 'cross' ? CROSS_REFERENCE_QUERY_NAME : PART_NUMBER_QUERY_NAME;
 
   const graphqlQuery = {
     query: `
-      query ${queryName}($q: String!, $limit: Int, $offset: Int, ${searchType === 'parts' ? ', $makeFilter: String, $modelFilter: String' : ''}${category ? ', $categoryFilter: String' : ''}${applyFuzziness ? ', $applyFuzziness: Boolean' : ''}) {
-        ${queryName}(q: $q, limit: $limit, offset: $offset, ${searchType === 'parts' ? ', makeFilter: $makeFilter, modelFilter: $modelFilter' : ''}${category ? ', categoryFilter: $categoryFilter' : ''}${applyFuzziness ? ', applyFuzziness: $applyFuzziness' : ''}) {
+      query ${queryName}($q: String!, $tenant: RcTenantEnum, $language: RcLocaleEnum, $limit: Int, $offset: Int, ${searchType === 'parts' ? ', $makeFilter: String, $modelFilter: String' : ''}${category ? ', $categoryFilter: String' : ''}${applyFuzziness ? ', $applyFuzziness: Boolean' : ''}) {
+        ${queryName}(q: $q, tenant: $tenant, language: $language, limit: $limit, offset: $offset, ${searchType === 'parts' ? ', makeFilter: $makeFilter, modelFilter: $modelFilter' : ''}${category ? ', categoryFilter: $categoryFilter' : ''}${applyFuzziness ? ', applyFuzziness: $applyFuzziness' : ''}) {
           count
           items {
             score
             uuid
             metadata {
+              tenant
               base_part_number
-              ${searchType === 'cross' ? 'mack_part_number volvo_part_number tenant manufacturer { oem_number name }' : 'model { description make name }'}
+              ${
+                searchType === 'cross'
+                  ? `
+                mack_part_number
+                volvo_part_number
+                manufacturer {
+                  oem_number
+                  name
+                }`
+                  : `
+                model {
+                  description
+                  make
+                  name
+                }`
+              }
               part_name
               path
               part_category
@@ -60,6 +89,8 @@ export async function fetchSearchResults({ query, offset, make, model, searchTyp
       }
     `,
     variables: {
+      tenant: TENANT,
+      language,
       q: query,
       limit: parseInt(MAX_PRODUCTS_PER_QUERY),
       offset,
@@ -79,13 +110,20 @@ export async function fetchSearchResults({ query, offset, make, model, searchTyp
   return { results, categories };
 }
 
-export async function fetchFilterFacets({ field, filter }) {
-  const { SEARCH_URL_DEV, FILTER_FACETS_QUERY_NAME } = SEARCH_CONFIG;
+/** Fetch filter facets from the GraphQL API based on the provided parameters.
+ * @param {Object} params - The filter parameters.
+ * @param {string} params.field - The field to fetch facets for (e.g., 'MAKE', 'MODEL').
+ * @param {string} [params.filter] - An optional filter string to narrow down the facets.
+ * @param {string} params.language - The language code for localization.
+ * @returns {Object} An object containing the facets and any error encountered.
+ */
+export async function fetchFilterFacets({ field, filter, language = getPageLanguage() }) {
+  const { SEARCH_URL_DEV, FILTER_FACETS_QUERY_NAME, TENANT } = SEARCH_CONFIG;
 
   const graphqlQuery = {
     query: `
-      query ${FILTER_FACETS_QUERY_NAME}($field: RcFieldEnum!, $filter: String) {
-        ${FILTER_FACETS_QUERY_NAME}(field: $field, filter: $filter) {
+      query ${FILTER_FACETS_QUERY_NAME}($field: RcFieldEnum!, $tenant: RcTenantEnum, $language: RcLocaleEnum, $filter: String) {
+        ${FILTER_FACETS_QUERY_NAME}(field: $field, tenant: $tenant, language: $language, filter: $filter) {
           facets {
             key
             doc_count
@@ -94,6 +132,8 @@ export async function fetchFilterFacets({ field, filter }) {
       }
     `,
     variables: {
+      tenant: TENANT,
+      language,
       field,
       ...(filter && { filter }),
     },
@@ -106,18 +146,27 @@ export async function fetchFilterFacets({ field, filter }) {
   return { facets: data.data[FILTER_FACETS_QUERY_NAME] };
 }
 
-export async function fetchCrossReferenceSuggest({ term, category }) {
-  const { SEARCH_URL_DEV, CROSS_REFERENCE_SUGGEST_QUERY_NAME, AUTOSUGGEST_SIZE_SUGGESTION } = SEARCH_CONFIG;
+/** Fetch auto-suggestions for part references or cross-references from the GraphQL API.
+ * @param {Object} params - The suggestion parameters.
+ * @param {string} params.term - The search term for which to fetch suggestions.
+ * @param {string} [params.category] - An optional category filter.
+ * @param {string} params.language - The language code for localization.
+ * @returns {Object} An object containing the suggestions and any error encountered.
+ */
+export async function fetchCrossReferenceSuggest({ term, category, language = getPageLanguage() }) {
+  const { SEARCH_URL_DEV, CROSS_REFERENCE_SUGGEST_QUERY_NAME, AUTOSUGGEST_SIZE_SUGGESTION, TENANT } = SEARCH_CONFIG;
 
   const graphqlQuery = {
     query: `
-      query ${CROSS_REFERENCE_SUGGEST_QUERY_NAME}($term: String!, $sizeSuggestions: Int${category ? ', $categoryFilter: String' : ''}) {
-        ${CROSS_REFERENCE_SUGGEST_QUERY_NAME}(term: $term, sizeSuggestions: $sizeSuggestions${category ? ', categoryFilter: $categoryFilter' : ''}) {
+      query ${CROSS_REFERENCE_SUGGEST_QUERY_NAME}($term: String!, $sizeSuggestions: Int${category ? ', $categoryFilter: String' : ''}, $tenant: RcTenantEnum, $language: RcLocaleEnum) {
+        ${CROSS_REFERENCE_SUGGEST_QUERY_NAME}(term: $term, sizeSuggestions: $sizeSuggestions${category ? ', categoryFilter: $categoryFilter' : ''}, tenant: $tenant, language: $language) {
           terms
         }
       }
     `,
     variables: {
+      tenant: TENANT,
+      language,
       term,
       sizeSuggestions: parseInt(AUTOSUGGEST_SIZE_SUGGESTION),
       ...(category && { categoryFilter: category }),
@@ -131,18 +180,29 @@ export async function fetchCrossReferenceSuggest({ term, category }) {
   return { suggestions: data.data[CROSS_REFERENCE_SUGGEST_QUERY_NAME] };
 }
 
-export async function fetchPartReferenceSuggest({ term, make, model, category }) {
-  const { SEARCH_URL_DEV, PART_REFERENCE_SUGGEST_QUERY_NAME, AUTOSUGGEST_SIZE_SUGGESTION } = SEARCH_CONFIG;
+/** Fetch fuzzy search suggestions from the GraphQL API based on the provided parameters.
+ * @param {Object} params - The fuzzy search parameters.
+ * @param {string} params.term - The search term for which to fetch suggestions.
+ * @param {string} [params.make] - An optional make filter.
+ * @param {string} [params.model] - An optional model filter.
+ * @param {string} [params.category] - An optional category filter.
+ * @param {string} params.language - The language code for localization.
+ * @returns {Object} An object containing the fuzzy search suggestions and any error encountered.
+ */
+export async function fetchPartReferenceSuggest({ term, make, model, category, language = getPageLanguage() }) {
+  const { SEARCH_URL_DEV, PART_REFERENCE_SUGGEST_QUERY_NAME, AUTOSUGGEST_SIZE_SUGGESTION, TENANT } = SEARCH_CONFIG;
 
   const graphqlQuery = {
     query: `
-      query ${PART_REFERENCE_SUGGEST_QUERY_NAME}($term: String!, $sizeSuggestions: Int${make ? ', $makeFilter: String' : ''}${model ? ', $modelFilter: String' : ''}${category ? ', $categoryFilter: String' : ''}) {
-        ${PART_REFERENCE_SUGGEST_QUERY_NAME}(term: $term, sizeSuggestions: $sizeSuggestions${make ? ', makeFilter: $makeFilter' : ''}${model ? ', modelFilter: $modelFilter' : ''}${category ? ', categoryFilter: $categoryFilter' : ''}) {
+      query ${PART_REFERENCE_SUGGEST_QUERY_NAME}($term: String!, $sizeSuggestions: Int${make ? ', $makeFilter: String' : ''}${model ? ', $modelFilter: String' : ''}${category ? ', $categoryFilter: String' : ''}, $tenant: RcTenantEnum, $language: RcLocaleEnum) {
+        ${PART_REFERENCE_SUGGEST_QUERY_NAME}(term: $term, sizeSuggestions: $sizeSuggestions${make ? ', makeFilter: $makeFilter' : ''}${model ? ', modelFilter: $modelFilter' : ''}${category ? ', categoryFilter: $categoryFilter' : ''}, tenant: $tenant, language: $language) {
           terms
         }
       }
     `,
     variables: {
+      tenant: TENANT,
+      language,
       term,
       sizeSuggestions: parseInt(AUTOSUGGEST_SIZE_SUGGESTION),
       ...(make && { makeFilter: make }),
@@ -158,13 +218,19 @@ export async function fetchPartReferenceSuggest({ term, make, model, category })
   return { suggestions: data.data[PART_REFERENCE_SUGGEST_QUERY_NAME] };
 }
 
-export async function fetchFuzzySuggest({ q }) {
-  const { SEARCH_URL_DEV, RC_PART_FUZZY_SEARCH, AUTOSUGGEST_SIZE_SUGGESTION } = SEARCH_CONFIG;
+/** Fetch fuzzy search suggestions from the GraphQL API based on the provided parameters.
+ * @param {Object} params - The fuzzy search parameters.
+ * @param {string} params.q - The search term for which to fetch suggestions.
+ * @param {string} params.language - The language code for localization.
+ * @returns {Object} An object containing the fuzzy search suggestions and any error encountered.
+ */
+export async function fetchFuzzySuggest({ q, language = getPageLanguage() }) {
+  const { SEARCH_URL_DEV, RC_PART_FUZZY_SEARCH, AUTOSUGGEST_SIZE_SUGGESTION, TENANT } = SEARCH_CONFIG;
 
   const graphqlQuery = {
     query: `
-      query ${RC_PART_FUZZY_SEARCH}($q: String!, $limit: Int) {
-        ${RC_PART_FUZZY_SEARCH}(q: $q,limit: $limit) {
+      query ${RC_PART_FUZZY_SEARCH}($q: String!, $limit: Int, $tenant: RcTenantEnum, $language: RcLocaleEnum) {
+        ${RC_PART_FUZZY_SEARCH}(q: $q, limit: $limit, tenant: $tenant, language: $language) {
           suggestions {
             highlighted
             text
@@ -173,6 +239,8 @@ export async function fetchFuzzySuggest({ q }) {
       }
     `,
     variables: {
+      tenant: TENANT,
+      language,
       q,
       limit: parseInt(AUTOSUGGEST_SIZE_SUGGESTION),
     },
