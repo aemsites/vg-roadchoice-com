210d2e4dfd8fabfb02782c8211f5155380396466
santi-homps <santiagohomps@gmail.com>
2025-10-24 11:07:17 -0300

    add consoles for checks

--- templates/part-category/part-category.js
+++ templates/part-category/part-category.js
@@ -62,12 +62,12 @@ const getCategory = () => {
 const getCategoryData = async (cat) => {
   try {
     const productDataUrl = getLocaleContextedUrl(`/product-data/rc-${cat.replace(/[^\w]/g, '-')}.json`);
-
+    console.log('url', productDataUrl);
     const products = await getLongJSONData({
       url: productDataUrl,
       limit: DEFAULT_LIMIT,
     });
-
+    console.log('products', products);
     if (!Array.isArray(products) || products.length === 0) {
       console.warn(`[CategoryData] No product data found or empty array returned for category: "${cat}"`);
       json.data = [];
@@ -112,7 +112,7 @@ const getFilterAttrib = async (cat) => {
       url: categoryMaster,
       limit: DEFAULT_LIMIT,
     });
-
+    console.log('filters', filtersJson);
     if (!filtersJson) throw new Error('Failed to fetch filter data');
 
     const filterAttribs = filtersJson
