bb759581147703f3d6178f5e01d9c62d59026568
Federico Nicolas Motta <fedemotta@gmail.com>
2026-02-18 20:01:19 -0300

    133131 - Bug: resolve Safari dealer locator rendering and iOS 17+ issues
    - Fix script loading order: HTML inserted before scripts load
    - Add jQuery ready check for Safari initialization
    - Fix map width issue with explicit CSS dimensions (left/right: 0)
    - Add geolocation options for iOS 17+ compatibility
    - Add Google Maps resize trigger for Safari rendering
    - Update sidebar-maps.js to use CORS proxy on localhost
    - Add permissions API check for geolocation error handling
    - Force map dimensions when Safari reports width: 0

--- blocks/dealer-locator/dealer-locator.js
+++ blocks/dealer-locator/dealer-locator.js
@@ -68,16 +68,6 @@ export default async function decorate(block) {
     };
   }
 
-  loadScript('/blocks/dealer-locator/vendor/jquery.min.js', { type: 'text/javascript', charset: 'UTF-8' }).then(() => {
-    // these scripts depend on jquery:
-    loadScript('/blocks/dealer-locator/sidebar-maps.js', { type: 'text/javascript', charset: 'UTF-8' });
-    loadScript('/blocks/dealer-locator/my-dealer.js', { type: 'text/javascript', charset: 'UTF-8' });
-  });
-
-  loadScript('/blocks/dealer-locator/vendor/moment.js', { type: 'text/javascript', charset: 'UTF-8' }).then(() => {
-    loadScript('/blocks/dealer-locator/vendor/moment-timezone.min.js', { type: 'text/javascript', charset: 'UTF-8' });
-  });
-
   block.innerHTML = `<input id="hoverText" value="Please unselect the selected option to click this option" hidden/>
 <div class="wrapper">
     <div class="mobile-main-header">
@@ -454,4 +444,15 @@ export default async function decorate(block) {
 <div id="locator-snackbar"></div>
 
 </div> `;
+
+  // Load scripts AFTER HTML is inserted in DOM
+  loadScript('/blocks/dealer-locator/vendor/jquery.min.js', { type: 'text/javascript', charset: 'UTF-8' }).then(() => {
+    // these scripts depend on jquery:
+    loadScript('/blocks/dealer-locator/sidebar-maps.js', { type: 'text/javascript', charset: 'UTF-8' });
+    loadScript('/blocks/dealer-locator/my-dealer.js', { type: 'text/javascript', charset: 'UTF-8' });
+  });
+
+  loadScript('/blocks/dealer-locator/vendor/moment.js', { type: 'text/javascript', charset: 'UTF-8' }).then(() => {
+    loadScript('/blocks/dealer-locator/vendor/moment-timezone.min.js', { type: 'text/javascript', charset: 'UTF-8' });
+  });
 }
